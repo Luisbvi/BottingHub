aa82b4e74561bfe0c122c2ba6078d22ec6399a87
Luisbvi <Luisbvigamer@gmail.com>
2021-06-16 10:45:05 -0400

    Starting

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler =>{
-    require(`./handlers/${handler}`)( client, Discord )
+    require(`./handlers/${handler}`)( client, Discord ) 
 });
 
 client.login(process.env.TOKEN);
