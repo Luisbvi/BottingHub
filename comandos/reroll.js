9c703557d0c47db7b8975969c08d01571646a852
Luisbvi <Luisbvigamer@gmail.com>
2021-06-19 19:41:18 -0400

    giveaway reroll+

---
+++ comandos/reroll.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'reroll',
+    alias:['roll'],
+    permissions:['ADMINISTATOR'],
+
+    execute(Discord,client,args){
+        const messageId = args[0]
+        client.giveawaysManager.reroll(messageId).then(() =>{
+            message.channel.send('Success! Giveaway rerolled!');
+        }).catch((err) =>{
+            message.channel.send('No giveaway found for ' + messageID + ', please check and try again');
+        });
+    }
+}
