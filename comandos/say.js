2007cc6ee22f14056caf9f4722e71470bb78f254
Luisbvi <Luisbvigamer@gmail.com>
2021-07-01 13:49:30 -0400

    giveaway

--- comandos/say.js
+++ comandos/say.js
@@ -3,10 +3,10 @@ module.exports = {
     alias: ["decir"], //Aquí un alias, esto será como un segundo nombre del comando, si no quieren ponerle alias tenéis que quitarle las " " y dejarlo así: alias: [],
     permissions: ["ADMINISTRATOR"],
 
-    execute (Discord, client , message, args){
+    async execute (Discord, client , message, args){
         let texto = args.join(' ');
         if (!texto) return message.channel.send('You need write a text!');
-        message.delete();
+        await message.delete();
         message.channel.send(texto);
     }
 }
--- comandos/status.js
+++ comandos/status.js
@@ -1,7 +1,6 @@
 const Canvas = require('canvas')
 const path = require('path')
 const Status = require('../models/Status');
-const { findOne } = require('../models/TicketConfig');
 module.exports = {
     name: ['status'],
     alias: ['s'],
@@ -12,20 +11,26 @@ module.exports = {
         const canvas = Canvas.createCanvas(765,503);
         const ctx = canvas.getContext('2d');
         const background = await Canvas.loadImage(
-            path.join(__dirname, '../background.png')
+            path.join(__dirname, '../backgroud.png')
         );
 
         ctx.font = '30px Impact'
-        ctx.fillStyle = '#ffffff'
+        ctx.fillStyle = '#000000'
         ctx.drawImage(background, 0,0, canvas.width, canvas.height)
         let Manager = 'BOT MANAGER STATUS'
         ctx.fillText(Manager, canvas.width / 2 - ctx.measureText(Manager).width / 2 , 100)
-        let creator = `Creator: ${status.Creator}`
-        ctx.fillText(creator, canvas.width / 2 - ctx.measureText(creator).width / 2 ,180)
-        let redeemer = ` Unlocker: ${status.Unlocker}`
-        ctx.fillText(redeemer, canvas.width / 2 - ctx.measureText(redeemer).width / 2 , 300)
-        let unlocker = `Redeemer: ${status.Redeemer}`
-        ctx.fillText(unlocker, canvas.width / 2 - ctx.measureText(unlocker).width / 2 , 420)
+        let creator = `Creator`
+        ctx.fillText(creator, 226, 170)
+        let redeemer = `Unlocker`
+        ctx.fillText(redeemer, 226 , 252)
+        let unlocker = `Redeemer`
+        ctx.fillText(unlocker, 226 , 338)
+        let creator1 = `${status.Creator}`
+        ctx.fillText(creator1, 415, 170)
+        let unlocker1 = `${status.Unlocker}`
+        ctx.fillText(unlocker1, 415 , 252)
+        let redeemer1 = `${status.Redeemer}`
+        ctx.fillText(redeemer1, 415 , 338)
         
     
 
